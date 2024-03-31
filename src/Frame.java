109b0ddbfbfd3b57d9d978fffc2291189f235aee
Monky518 <zoeyrizor@gmail.com>
2024-03-31 19:20:44 -0400

    Grab user text file and read

---
+++ src/Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Frame extends JFrame
+{
+    public Frame()
+    {
+        setTitle("Rock Paper Scissors");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // JFrame mainFrame = new Frame();
+        JPanel mainPanel = new JPanel();
+
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile; // why do I need this?
+        String rec = "";
+        ArrayList<String> test = new ArrayList<>();
+
+        try {
+            // File workingDirectory = new File(System.getProperty("user.dir"));
+            // chooser.setCurrentDirectory(workingDirectory);
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    test.add(rec);
+                }
+                reader.close();
+                System.out.println("Test Read\n---------\n");
+                String[] keyWords;
+                for(String l : test) {
+                    keyWords = l.split(" ");
+                    for (String cut : keyWords)
+                    {
+                        // cuts noise words
+                        // cuts non-letter (regex)
+                    }
+                    System.out.println(l);
+                }
+                System.out.println("\n---------");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("\nERROR: File not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
